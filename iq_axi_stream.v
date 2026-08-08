6d4699c6192a6cb85b0bec9121d8e259d92d1dd2
CarmelToaster <allenconnor304@gmail.com>
2026-08-08 15:21:32 -0500

    Create iq_axi_stream.v

---
+++ iq_axi_stream.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module iq_axi_stream #(
+    parameter PW = 32,
+    parameter ADDR_WIDTH = 8
+)(
+    input  wire clk,
+    input  wire rst,
+    input  wire [PW-1:0] phase_step,
+    output wire [31:0] tdata,
+    output wire tvalid,
+    input  wire tready
+);
